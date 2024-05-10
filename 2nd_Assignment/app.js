b72eee5e68a5f4c55d2795e3b579f6c2293489d8
Hameer Ahmed <78217588+Hameer-Ahmed@users.noreply.github.com>
2024-05-10 20:11:54 +0500

    Add files via upload

---
+++ 2nd_Assignment/app.js
@@ -0,0 +1,74 @@
+// --------------Chapter Node.2--------------
+
+// Q:1. Declare a variable called username.
+
+// let username;
+
+// Q:2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// let myName = "Hameer Ahmed";
+
+// Q:3. Write script to
+
+// let message = "Hello World";
+// alert(message);
+
+// Q:4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// let StudentName = "Hameer Ahmed";
+// let StudentAge = 20;
+// let StudentGrade = "A";
+// let studentBio =
+//   "Name: " + StudentName + "\nAge: " + StudentAge + "\nGrade: " + StudentGrade;
+// alert(studentBio);
+
+// Q:5 Write a script to display the following alert using one JS variable:
+
+// var text = "PIZZA";
+// var output = "";
+// for (var i = text.length; i > 0; i--) {
+//     output += text.substring(0, i) + "\n";
+// }
+// alert(output);
+
+// Q:6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+// let email = "hameer.langah@gamil.com";
+// let message = "My Email Adress is: " + email;
+// alert(message);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+
+// let book = "A smarter way to learn JavaScript";
+// alert(book);
+
+// 8.Write a script to display this in browser through JS
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// 9. Store following string in a variable and show in alert and browser through JS
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+// ----------------------chapter No.3-------------------------------
+
+/*Q:1
+var age = 30;
+alert("I am " + age + " years old.");*/
+
+// Q:2
+
+// Q:3.
+// var birthYear = 1994;
+// console.log(
+//   "My birth year is " +
+//     birthYear +
+//     "\nData type of my declared veriable is number"
+// );
+
+//Q:4
+// var visiterName= "John Doe";
+// var ProductTitle= "T-shirt(s)";
+// var Quantity= 5;
+
+// var message= visiterName + " Order" + Quantity + " " + ProductTitle + "  on XYZ Clothing store.";
+// document.write(message);

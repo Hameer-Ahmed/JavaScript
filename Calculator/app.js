5a5c121368aaabbd8092a430b73251e942ca6ce8
Hameer Ahmed <78217588+Hameer-Ahmed@users.noreply.github.com>
2024-05-21 21:49:23 +0500

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,20 @@
+let num1 = +prompt("Enter Number 1");
+let num2 = +prompt("Enter Number 2");
+document.write("Value of num1 = ", num1 + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp");
+document.write("Value of num2 = ", num2, "<br>");
+document.write("=============================", "<br>");
+
+let sum = num1 + num2;
+document.write("Sum of two numbers: ", sum, "<br><br>");
+
+let sub = num1 - num2;
+document.write("Subtraction of two numbers: ", sub, "<br><br>");
+
+let multi = num1 * num2;
+document.write("Multipliction of two numbers: ", multi, "<br><br>");
+
+let div = num1 / num2;
+document.write("Division of two numbers: ", div, "<br><br>");
+
+let reminder = num1 % num2;
+document.write("Reminder of two numbers: ", reminder, "<br><br>");
